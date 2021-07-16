4c9a7a740a624ed6003a321187232a60dfa57580
megaketchup <megaketchup@hotmail.com>
2021-07-16 15:48:21 -0500

    w
    w

---
+++ Assets/char_control.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimRotation();
+    }
+     void AimRotation()
+    {
+        if (true)
+        {
+            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+            Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            Vector3 direction = mouseOnScreen - positionOnScreen;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+        }
+    }
+}

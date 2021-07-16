686905ad0c744e00e471e28033ae1d2dd66dec27
megaketchup <megaketchup@hotmail.com>
2021-07-16 16:06:42 -0500

    f
    f

--- Assets/char_control.cs
+++ Assets/char_control.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class char_control : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject body;
+    public float speed=1;
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         AimRotation();
+        PlayerMovement();
     }
      void AimRotation()
     {
         if (true)
         {
-            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(body.transform.position);
             Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
             Vector3 direction = mouseOnScreen - positionOnScreen;
 
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
-            transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+            body.transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+        }
+    }
+    void PlayerMovement()
+    {
+        if(true)
+        {
+            
+           Vector3 direction = new Vector3((-1*Input.GetAxisRaw("Vertical")), 0, Input.GetAxisRaw("Horizontal")).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                 GetComponent<CharacterController>().Move(direction * speed * Time.deltaTime);                 
+            }
+            
+
+        
         }
     }
 }

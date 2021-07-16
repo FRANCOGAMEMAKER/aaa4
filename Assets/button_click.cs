195ca1532c67564a153934b5fb3afb2a50db9e55
megaketchup <megaketchup@hotmail.com>
2021-07-16 17:09:36 -0500

    g
    g

--- Assets/button_click.cs
+++ Assets/button_click.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class button_click : MonoBehaviour
 {
+public Canvas parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,9 @@ public class button_click : MonoBehaviour
     void Update()
     {
         
+    }
+    public void shrink()
+    {
+      parent.transform.localScale=new Vector3(0,0,0);
     }
 }
